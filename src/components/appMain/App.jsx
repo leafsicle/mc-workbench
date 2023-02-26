7abbcd1fd7af7dbde24cb545b700fe3d831d6cfc
MattCooke <cookem529@gmail.com>
2023-02-26 16:07:20 -0500

    adding router capabilities

--- src/components/appMain/App.jsx
+++ src/components/appMain/App.jsx
@@ -1,12 +1,54 @@
-import { useState } from 'react'
+import { Routes, Route, Outlet, Link } from 'react-router-dom'
 import './App.css'
+import Dashboard from '../dashboard'
+import About from '../about'
+import NotFound from '../404'
 
-const App = () => {
+export default function App () {
   return (
-    <div className='App'>
-      <h1>Oh hi</h1>
+    <div>
+      {/* Routes nest inside one another. Nested route paths build upon
+            parent route paths, and nested route elements render inside
+            parent route elements. See the note about <Outlet> below. */}
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<About />} />
+          <Route path='dashboard' element={<Dashboard />} />
+
+          {/* Using path="*"" means "match anything", so this route
+                acts like a catch-all for URLs that we don't have explicit
+                routes for. */}
+          <Route path='*' element={<NotFound />} />
+        </Route>
+      </Routes>
     </div>
   )
 }
 
-export default App
+function Layout () {
+  return (
+    <div className='layout'>
+      {/* A "layout route" is a good place to put markup you want to
+          share across all the pages on your site, like navigation. */}
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/dashboard'>Dashboard</Link>
+          </li>
+          <li>
+            <Link to='/nothing-here'>Click to see 404 for debug</Link>
+          </li>
+        </ul>
+      </nav>
+      <hr style={{ margin: '2rem 0' }} />
+
+      {/* An <Outlet> renders whatever child route is currently active,
+          so you can think about this <Outlet> as a placeholder for
+          the child routes we defined above. */}
+      <Outlet />
+    </div>
+  )
+}
