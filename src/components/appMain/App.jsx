ba5c2ca0e1a7754a93010c9041d09060425f6a81
MattCooke <cookem529@gmail.com>
2023-02-26 16:11:38 -0500

    extracts header component

--- src/components/appMain/App.jsx
+++ src/components/appMain/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Dashboard from '../dashboard'
 import About from '../about'
 import NotFound from '../404'
+import Header from '../header'
 
 export default function App () {
   return (
@@ -11,7 +12,7 @@ export default function App () {
             parent route paths, and nested route elements render inside
             parent route elements. See the note about <Outlet> below. */}
       <Routes>
-        <Route path='/' element={<Layout />}>
+        <Route path='/' element={<Header />}>
           <Route index element={<About />} />
           <Route path='dashboard' element={<Dashboard />} />
 
@@ -21,34 +22,6 @@ export default function App () {
           <Route path='*' element={<NotFound />} />
         </Route>
       </Routes>
-    </div>
-  )
-}
-
-function Layout () {
-  return (
-    <div className='layout'>
-      {/* A "layout route" is a good place to put markup you want to
-          share across all the pages on your site, like navigation. */}
-      <nav>
-        <ul>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/dashboard'>Dashboard</Link>
-          </li>
-          <li>
-            <Link to='/nothing-here'>Click to see 404 for debug</Link>
-          </li>
-        </ul>
-      </nav>
-      <hr style={{ margin: '2rem 0' }} />
-
-      {/* An <Outlet> renders whatever child route is currently active,
-          so you can think about this <Outlet> as a placeholder for
-          the child routes we defined above. */}
-      <Outlet />
     </div>
   )
 }
