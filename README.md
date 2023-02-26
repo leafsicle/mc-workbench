76caa74ca676e3420d68c02c4c818b03f85cb549
MattCooke <cookem529@gmail.com>
2023-02-26 15:33:47 -0500

    static

---
+++ README.md
@@ -0,0 +1,9 @@
+#### Running Locally
+
+##### Mobile
+
+```bash
+    yarn run dev --host
+```
+
+then select the Network address: and navigate to it on your device
---
+++ src/components/appMain/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='App'>
+      <h1>Oh hi</h1>
+    </div>
+  )
+}
+
+export default App
