425d812688569115d1aeffe3bd3b3da2559d0f0a
Matt Cooke <cookem529@gmail.com>
2024-07-05 23:12:38 -0400

    remove unused base

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: 3001 },
-  base: process.env.NODE_ENV === 'production' ? '/mc-workbench/' : '/'
+  server: { port: 3001 }
 })
