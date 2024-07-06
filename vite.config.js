6dbc96a46ebf7ff6ee073a02ff5b03f052b883d1
Matt Cooke <cookem529@gmail.com>
2024-07-05 20:24:19 -0400

    chg prefix

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: { port: 3001 },
-  base: '/mc-workbench/'
+  base: process.env.NODE_ENV === 'production' ? '/mc-workbench/' : '/'
 })
