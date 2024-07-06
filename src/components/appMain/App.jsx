94c0a211f7565041a63e48edeae468b7e084f945
Matt Cooke <cookem529@gmail.com>
2024-07-05 20:28:16 -0400

    re add router

--- src/components/appMain/App.jsx
+++ src/components/appMain/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 // import Garden from '../garden/Garden'
 import ContactForm from '../contactForm/ContactForm'
 import Darkness from '../darkness/Darkness'
@@ -18,15 +18,17 @@ export default function App () {
         {/* Routes nest inside one another. Nested route paths build upon
             parent route paths, and nested route elements render inside
             parent route elements.  <Outlet> is child. */}
-        <Routes>
-          <Route path='/' element={<Header />}>
-            <Route index element={<About />} />
-            {/* <Route path='/garden' element={<Garden />} /> */}
-            <Route path='/contact' element={<ContactForm />} />
-            <Route path='/calculators' element={<Calculators />} />
-            <Route path='*' element={<Darkness />} />
-          </Route>
-        </Routes>
+        <Router basename='/mc-workbench'>
+          <Routes>
+            <Route path='/' element={<Header />}>
+              <Route index element={<About />} />
+              {/* <Route path='/garden' element={<Garden />} /> */}
+              <Route path='/contact' element={<ContactForm />} />
+              <Route path='/calculators' element={<Calculators />} />
+              <Route path='*' element={<Darkness />} />
+            </Route>
+          </Routes>
+        </Router>
       </ThemeProvider>
     </div>
   )
