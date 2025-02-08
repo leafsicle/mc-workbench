151b1c9056be4242ff37121fed936a52de61dc72
Matt Cooke <cookem529@gmail.com>
2025-02-08 12:21:07 -0500

    Add ESLint and Prettier configuration files for code quality enforcement

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,41 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     {
-      name: 'remove-client-directive',
-      transform (code, id) {
-        if (id.includes('node_modules/@mui/base')) {
-          return code.replace(/"use client";?/g, '')
+      name: "remove-client-directive",
+      transform(code, id) {
+        if (
+          id.includes("node_modules/@mui/base") ||
+          id.includes("node_modules/@mui/material") ||
+          id.includes("node_modules/@mui/lab")
+        ) {
+          return {
+            code: code.replace(/"use client";?/g, ""),
+            map: null
+          }
         }
       }
     }
   ],
   server: { port: 3001 },
-  assetsInclude: ['**/*.csv'],
-  base: './',
+  assetsInclude: ["**/*.csv"],
+  base: "./",
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
+      "@": path.resolve(__dirname, "./src")
+    }
+  },
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === "MODULE_LEVEL_DIRECTIVE") return
+        warn(warning)
+      }
     }
   }
 })
